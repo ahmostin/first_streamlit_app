05b68ba5f809aed0a3de7f4ba71e457e4a6f633d
ahmostin <ah_mostin@yahoo.com>
2023-06-07 12:43:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
 streamlit.title('Hello the world, I am home')
+ 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔  Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
