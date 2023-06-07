8c57789530b886a106499ce38a0c5bb7cfaa827a
ahmostin <ah_mostin@yahoo.com>
2023-06-07 12:44:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Hello the world, I am home')
  
@@ -10,3 +11,5 @@ streamlit.text('🐔  Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
